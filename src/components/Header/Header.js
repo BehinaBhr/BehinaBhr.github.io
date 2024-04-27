86b65f6dbeb9019eb2daf7ee430f0e25b1c4e2a2
BehinaBhr <be.bhr77@gmail.com>
2024-04-26 20:52:25 -0700

    Implement responsive header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,31 @@ import "./Header.scss";
 import { Link } from "react-router-dom";
 
 function Header() {
-  return Header;
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <Link to="/">
+         <h2 className="header__logo-text"> {"< Behina Bhr />"}</h2>
+        </Link>
+      </div>
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <Link to={"/"}>
+            <li className="header__nav-item">About</li>
+          </Link>
+          <Link to={"/skill"}>
+            <li className="header__nav-item">Skill</li>
+          </Link>
+          <Link to={"/experience"}>
+            <li className="header__nav-item">Experience</li>
+          </Link>
+          <Link to={"/contact"}>
+            <li className="header__nav-item">Contact</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
