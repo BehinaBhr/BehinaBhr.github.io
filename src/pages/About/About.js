85c6231caf2a673c6d54830b02dd4ad13181fe05
BehinaBhr <be.bhr77@gmail.com>
2024-05-27 18:19:46 -0700

    Remove scrolling feature from About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,20 +6,8 @@ import Intro from "../../components/Intro/Intro";
 
 const About = () => {
   DocumentTitle("About Me");
-  // Target element ID for scrolling
-  const scrollToTarget = () => {
-    const element = document.getElementById("about");
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-  // Call scrollToTarget on component mount for automatic scrolling
-  useEffect(() => {
-    scrollToTarget(); // Uncomment to enable automatic scrolling on load
-  }, []);
-
   return (
-    <div className="about" id="about">
+    <div className="about">
       <div className="about__summary">
         <Summary />
       </div>
