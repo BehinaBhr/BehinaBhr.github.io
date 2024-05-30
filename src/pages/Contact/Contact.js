8605fba951257ef2dd531be5e5f12a360b4ecad2
BehinaBhr <be.bhr77@gmail.com>
2024-05-30 16:03:41 -0700

    Style contact page fully responsive and add ContactLink

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,7 +2,7 @@ import "./Contact.scss";
 import { DocumentTitle } from "../../utils/utils";
 import { useState, useRef, useEffect } from "react";
 import emailjs from "@emailjs/browser";
-// import ContactLink from "../../components/ContanctLink/ContactLink";
+import ContactLink from "../../components/ContanctLink/ContactLink";
 
 const Contact = () => {
   DocumentTitle("Contact Me");
@@ -94,70 +94,64 @@ const Contact = () => {
 
   return (
     <div className="contact">
-      <div className="contact__slogan">
-        <img
-          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXN5eG5ybmMwbGZhN2lzcWdoYXd5NW1pZWFmODN4OXNhYWYzbHhsOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/cCLA9VfpjsgW3nE0oi/giphy.gif"
-          alt="an animated mailbox opening with letters coming out"
-          loading="lazy"
-        />
+      <div className="contact__img-wrapper">
+        <div className="contact__slogan">
+          <img
+            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXN5eG5ybmMwbGZhN2lzcWdoYXd5NW1pZWFmODN4OXNhYWYzbHhsOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/cCLA9VfpjsgW3nE0oi/giphy.gif"
+            alt="an animated mailbox opening with letters coming out"
+            loading="lazy"
+          />
+        </div>
+        <ContactLink />
       </div>
       <div className="contact__content-wrapper">
-        <h2 className="contact__title">Let's Connect!</h2>
+        <h2 className="contact__title">Let's Reach Out Together!</h2>
         <p className="contact__info">
           Whether you'd like to
           <span className="contact__info--bold"> share your story, discuss a collaboration, or provide feedback, </span>
           your insights are invaluable to me. Looking forward to hearing from you.
         </p>
-        <div className="contact__form">
-          <form ref={form} onSubmit={handleSubmit}>
-            <div className="contact__form-group">
-              <label htmlFor="user_name">Hi, There! My Name is...</label>
-              <input
-                type="text"
-                id="user_name"
-                name="user_name"
-                required
-                value={formData.user_name}
-                onChange={handleChange}
-              />
-              {errors.user_name && <span className="error">{errors.user_name}</span>}
-            </div>
-            <div className="contact__form-group">
-              <label htmlFor="user_email">My Email:</label>
-              <input
-                type="email"
-                id="user_email"
-                name="user_email"
-                required
-                value={formData.user_email}
-                onChange={handleChange}
-              />
-              {errors.user_email && <span className="error">{errors.user_email}</span>}
-            </div>
-            <div className="contact__form-group">
-              <label htmlFor="message">My Message:</label>
-              <textarea
-                id="message"
-                name="message"
-                rows="4"
-                required
-                value={formData.message}
-                onChange={handleChange}
-              />
-              {errors.message && <span className="error">{errors.message}</span>}
-            </div>
-            {successMessageVisible ? null : (
-              <div className="contact__form-button">
-                <button type="submit">{formSubmitted ? "Send Another Message" : "Send to Behina"}</button>
-              </div>
-            )}
-          </form>
-          {successMessageVisible && (
-            <div className="contact__form-success-message">
-              <p>Thank you for your message! I'll get back to you soon.</p>
+        <form className="contact__form" ref={form} onSubmit={handleSubmit}>
+          <div className="contact__form-group">
+            <label htmlFor="user_name">Hi, There! My Name is...</label>
+            <input
+              type="text"
+              id="user_name"
+              name="user_name"
+              required
+              value={formData.user_name}
+              onChange={handleChange}
+            />
+            {errors.user_name && <span className="error">{errors.user_name}</span>}
+          </div>
+          <div className="contact__form-group">
+            <label htmlFor="user_email">My Email:</label>
+            <input
+              type="email"
+              id="user_email"
+              name="user_email"
+              required
+              value={formData.user_email}
+              onChange={handleChange}
+            />
+            {errors.user_email && <span className="error">{errors.user_email}</span>}
+          </div>
+          <div className="contact__form-group">
+            <label htmlFor="message">My Message:</label>
+            <textarea id="message" name="message" rows="4" required value={formData.message} onChange={handleChange} />
+            {errors.message && <span className="error">{errors.message}</span>}
+          </div>
+          {successMessageVisible ? null : (
+            <div className="contact__form-button">
+              <button type="submit">{formSubmitted ? "Send Another Message" : "Send to Behina"}</button>
             </div>
           )}
-        </div>
+        </form>
+        {successMessageVisible && (
+          <div className="contact__success-message">
+            <p>Thank you for your message! I'll get back to you soon.</p>
+          </div>
+        )}
       </div>
     </div>
   );
