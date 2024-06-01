832246693d00330e6d4f24bf63d3efd25fec2d82
BehinaBhr <be.bhr77@gmail.com>
2024-05-31 17:29:03 -0700

    Update logic of timeline component

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,80 +1,48 @@
 import "./Timeline.scss";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import SchoolIcon from "../../assets/images/school.svg";
-import WorkIcon from "../../assets/images/work.svg";
-
-
-const WorkTimelineElement = ({ date, title, subtitle, description }) => (
-  <VerticalTimelineElement className="timeline-element timeline-element--work" date={date} icon={<img src={WorkIcon} alt="Work Icon" />}>
-    <h3 className="timeline-element-title">{title}</h3>
-    <h4 className="timeline-element-subtitle">{subtitle}</h4>
-    <p>{description}</p>
-  </VerticalTimelineElement>
-);
-
-const EducationTimelineElement = ({ date, title, subtitle, description }) => (
-  <VerticalTimelineElement className="timeline-element timeline-element--education" date={date} icon={<img src={SchoolIcon} alt="School Icon" />}>
-    <h3 className="timeline-element-title">{title}</h3>
-    <h4 className="timeline-element-subtitle">{subtitle}</h4>
-    <p>{description}</p>
-  </VerticalTimelineElement>
-);
+import TimelineElements from "./TimelineElements";
+import { ReactComponent as WorkIcon } from "../../assets/images/work.svg";
+import { ReactComponent as SchoolIcon } from "../../assets/images/school.svg";
 
 const Timeline = () => {
   return (
-    <div className="timeline">
-      <VerticalTimeline>
-        <WorkTimelineElement
-          date="Jan 2023 - Nov 2023"
-          title="Customer Service Sales Specialist"
-          subtitle="Sobeys"
-          description="Consulted HR/I-O Psychology insights to optimize high-volume requests, fostering collaboration and personalized client relations, boosting satisfaction and sales. Founded a customer-centric atmosphere with organized displays, resolved upset customers' issues, and ensured precision in online support, improving overall service quality."
-        />
-        <WorkTimelineElement
-          date="Mar 2021 - Apr 2022"
-          title="Human Resources Assistant"
-          subtitle="The Shining Sun - Banquet Hall and Restaurant"
-          description="Led end-to-end recruitment processes, optimizing personnel actions and fostering a positive work environment by leveraging I-O Psychology. Managed HR operations, streamlining administrative tasks, maintaining office organization, and drafting documents using Microsoft Office. Achieved increased sales and brand awareness through enhanced social media communication with clients."
-        />
-        <WorkTimelineElement
-          date="May 2019 - May 2021"
-          title="Sales and Customer Service Specialist"
-          subtitle="Peyk Saba - Travel Company"
-          description="Consulted and welcomed customers with a positive first impression, offering personalized plans with product suggestions and special promotions by analyzing their financial goals and preferences to foster long-term client relationships while implementing pandemic safety protocols thereby preventing a decrease in sales and achieving company targets. Led the successful transition to online sales during the COVID-19 pandemic by adeptly guiding customers, offering multilingual support (English and Persian) across various channels, and promptly addressing inquiries and feedback to prioritize customer satisfaction and significantly increase e-commerce sales."
-        />
-        <WorkTimelineElement
-          date="Apr 2019 - Apr 2021"
-          title="Volunteer Assistant Psychologist"
-          subtitle="Mahak - International Society for Children with Cancer"
-          description="Provided guided psychological services with tailored interventions for children and families, demonstrating empathetic communication in high-pressure situations."
-        />
-        <WorkTimelineElement
-          date="Sep 2013 - Dec 2014"
-          title="Co-op Robotic Rescue Team"
-          subtitle="Farzanegan 1 Highschool"
-          description="Led a team in integrating robotics and programming C++ expertise to develop simulators and create intelligent robots for navigating intricate mazes and avoiding obstacles. Won 1st Place and 3rd Place in the Junior Robotic Competitions."
-        />
-        <EducationTimelineElement
-          date="Jan 2024 - Apr 2024"
-          title="GradDip, Software Engineering"
-          subtitle="BrainStation"
-          description='<a href="https://drive.google.com/file/d/1fqzRC7oBGoGvTlqTCSbTU84DUgbZ9m4b/view?usp=sharing" target="_blank">View Certificate</a>'
-        />
-        <EducationTimelineElement
-          date="Sep 2017 - Apr 2021"
-          title="BSc, Psychology"
-          subtitle="Bu-Ali Sina University"
-          description='<a href="https://drive.google.com/file/d/1az-R3H1IA4Lc4EIm0DZKJZszNQFECUEc/view?usp=sharing" target="_blank">View Certificate</a>'
-        />
-        <EducationTimelineElement
-          date="Sep 2012 - Jan 2017"
-          title="Dip, Experimental Science"
-          subtitle="National Organization for Development of Exceptional Talents"
-          description='<a href="https://drive.google.com/file/d/16zyLz2RPhp_ohtxOUpEYtNFw0OWqqNNo/view?usp=sharing" target="_blank">View Certificate</a>'
-        />
-      </VerticalTimeline>
-    </div>
+    <VerticalTimeline>
+      {TimelineElements.map((element) => {
+        let isWorkIcon = element.icon === "work";
+        let iconClass = isWorkIcon ? "__work" : "__school";
+        let showButton = !!element.buttonText;
+        let buttonClass = `vertical-timeline-element-button ${
+          isWorkIcon ? "vertical-timeline-element-button__work" : "vertical-timeline-element-button__school"
+        }`;
+        let elementClass = isWorkIcon ? "__work" : "__school";
+
+        return (
+          <VerticalTimelineElement
+            key={element.id}
+            iconClassName={`vertical-timeline-element-icon${iconClass}`}
+            icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+            date={element.date}
+            className={`vertical-timeline-element${elementClass}`}
+          >
+            <h3 className="vertical-timeline-element-title">{element.title}</h3>
+            <h5 className="vertical-timeline-element-location">{element.location}</h5>
+            <p className="vertical-timeline-element-description">{element.description}</p>
+            {showButton && (
+              <a
+                className={buttonClass}
+                href={element.buttonLink}
+                // to open the linked document in a new tab
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {element.buttonText}
+              </a>
+            )}
+          </VerticalTimelineElement>
+        );
+      })}
+    </VerticalTimeline>
   );
 };
 
---
+++ src/components/Timeline/TimelineElements.js
@@ -0,0 +1,78 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Customer Service Sales Specialist",
+    location: "Sobeys",
+    description:
+      "Consulted HR/I-O Psychology insights to optimize high-volume requests, fostering collaboration and personalized client relations, boosting satisfaction and sales. Founded a customer-centric atmosphere with organized displays, resolved upset customers' issues, and ensured precision in online support, improving overall service quality.",
+    date: "Jan 2023 - Nov 2023",
+    icon: "work",
+    buttonText: "more",
+    buttonLink: "https://drive.google.com/file/d/16zyLz2RPhp_ohtxOUpEYtNFw0OWqqNNo/view?usp=sharing",
+  },
+  {
+    id: 2,
+    title: "Human Resources Assistant",
+    location: "The Shining Sun - Banquet Hall and Restaurant",
+    description:
+      "Led end-to-end recruitment processes, optimizing personnel actions and fostering a positive work environment by leveraging I-O Psychology. Managed HR operations, streamlining administrative tasks, maintaining office organization, and drafting documents using Microsoft Office. Achieved increased sales and brand awareness through enhanced social media communication with clients.",
+    date: "Mar 2021 - Apr 2022",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "Sales and Customer Service Specialist",
+    location: "Peyk Saba - Travel Company",
+    description:
+      "Consulted and welcomed customers with a positive first impression, offering personalized plans with product suggestions and special promotions by analyzing their financial goals and preferences to foster long-term client relationships while implementing pandemic safety protocols thereby preventing a decrease in sales and achieving company targets. Led the successful transition to online sales during the COVID-19 pandemic by adeptly guiding customers, offering multilingual support (English and Persian) across various channels, and promptly addressing inquiries and feedback to prioritize customer satisfaction and significantly increase e-commerce sales.",
+    date: "May 2019 - May 2021",
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "Volunteer Assistant Psychologist",
+    location: "Mahak - International Society for Children with Cancer",
+    description:
+      "Provided guided psychological services with tailored interventions for children and families, demonstrating empathetic communication in high-pressure situations.",
+    date: "Apr 2019 - Apr 2021",
+    icon: "work",
+  },
+  {
+    id: 5,
+    title: "Co-op Robotic Rescue Team",
+    location: "Farzanegan 1 Highschool",
+    description:
+      "Led a team in integrating robotics and programming C++ expertise to develop simulators and create intelligent robots for navigating intricate mazes and avoiding obstacles. Won 1st Place and 3rd Place in the Junior Robotic Competitions.",
+    date: "Sep 2013 - Dec 2014",
+    icon: "work",
+  },
+  {
+    id: 6,
+    title: "GradDip, Software Engineering",
+    location: "BrainStation",
+    date: "Jan 2024 - Apr 2024",
+    icon: "school",
+    buttonText: "My Certificate",
+    buttonLink: "https://drive.google.com/file/d/1fqzRC7oBGoGvTlqTCSbTU84DUgbZ9m4b/view?usp=sharing",
+  },
+  {
+    id: 7,
+    title: "BSc, Psychology",
+    location: "Bu-Ali Sina University",
+    date: "Sep 2017 - Apr 2021",
+    icon: "school",
+    buttonText: "My  Certificate",
+    buttonLink: "https://drive.google.com/file/d/1az-R3H1IA4Lc4EIm0DZKJZszNQFECUEc/view?usp=sharing",
+  },
+  {
+    id: 8,
+    title: "Dip, Experimental Science",
+    location: "National Organization for Development of Exceptional Talents",
+    date: "Sep 2012 - Jan 2017",
+    icon: "school",
+    buttonText: "My  Certificate",
+    buttonLink: "https://drive.google.com/file/d/16zyLz2RPhp_ohtxOUpEYtNFw0OWqqNNo/view?usp=sharing",
+  },
+];
+
+export default timelineElements;
