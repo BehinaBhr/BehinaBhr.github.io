bdf7e6e7d565e2db249f2e74d7828c043be23798
BehinaBhr <be.bhr77@gmail.com>
2024-06-04 17:36:09 -0700

    Add header and list styling to certificate component

--- src/components/Certificate/Certificate.js
+++ src/components/Certificate/Certificate.js
@@ -1,22 +1,27 @@
 import "./Certificate.scss";
+import certificateIcon from "../../assets/images/certificate-wide.svg";
 import CertificateSlider from "../CertificateSlider/CertificateSlider";
 import CertificateElements from "../CertificateSlider/CertificateElements";
 
 const Certificate = () => {
   return (
     <div className="certificate">
-      <section className="certificate__list">
-        <ul>
-          <li>100 Days of Code: The Complete Python Pro Bootcamp - Udemy</li>
-          <li>Crash Course on Python - Grow with Google on Coursera</li>
-          <li>Python for Non-Programmers - LinkedIn</li>
-          <li>The 7th brain & cognition school - Brain & Cognition Clinic (Bacogc)</li>
-          <li>
-            The spring school on Artificial intelligence, philosophy, ethics & society - Sharif University of Technology
-          </li>
-        </ul>
-      </section>
-      <CertificateSlider data={CertificateElements} />
+      <div className="certificate__header">
+        <img className="certificate__header-icon" src={certificateIcon} alt="Certificate icon" />
+        <h2 className="certificate__header-title">Professional Certificates</h2>
+      </div>
+      <ul className="certificate__list">
+        <li>100 Days of Code: The Complete Python Pro Bootcamp - Udemy</li>
+        <li>Crash Course on Python - Grow with Google on Coursera</li>
+        <li>Python for Non-Programmers - LinkedIn</li>
+        <li>The 7th brain & cognition school - Brain & Cognition Clinic (Bacogc)</li>
+        <li>
+          The spring school on Artificial intelligence, philosophy, ethics & society - Sharif University of Technology
+        </li>
+      </ul>
+      <div className="certificate__slider">
+        <CertificateSlider data={CertificateElements} />
+      </div>
     </div>
   );
 };
--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -1,6 +1,5 @@
 import "./Experience.scss";
 import { DocumentTitle } from "../../utils/utils";
-import experienceIcon from "../../assets/images/experience.svg";
 import Timeline from "../../components/Timeline/Timeline";
 import Certificate from "../../components/Certificate/Certificate";
 
@@ -8,15 +7,12 @@ const Experience = () => {
   DocumentTitle(" My Experience");
   return (
     <div className="experience">
-      <div className="experience__header">
-        <img className="experience__header-icon" src={experienceIcon} alt="experience icon" />
-        <h2 className="experience__header-title">My Educational & Career Path</h2>
-      </div>
+      <h2 className="experience__header">My Educational & Career Path</h2>
       <section className="experience__section">
-      <Timeline />
+        <Timeline />
       </section>
       <section className="experience__section">
-      <Certificate />
+        <Certificate />
       </section>
     </div>
   );
