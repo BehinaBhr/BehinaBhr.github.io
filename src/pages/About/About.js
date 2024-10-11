9cd8ea3b9de39fea5ced4cd6c2bec47847555be9
BehinaBhr <be.bhr77@gmail.com>
2024-10-10 23:32:51 -0700

    Apply initial improved styling to the about page.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,31 +4,34 @@ import baby from "../../assets/images/baby.png";
 
 const About = () => {
   DocumentTitle("About Me");
+
   return (
     <div className="about">
       <h2 className="about__title">🙋🏻‍♀️ My Story</h2>
-      <img className="about__slogan" src={baby} alt="baby girl" />
-      <p className="about__content">
-        I was born with a name meaning "the best" in Persian, yet my path has been one of conquering perfectionism,
-        embracing continuous growth, and cherishing lifelong learning. My career exploration across robotics,
-        psychology, human resources, customer service, and sales has equipped me with a unique blend of analytical
-        thinking, leadership, and interpersonal communication skills. These transferable skillsets, honed through
-        academic, fast-paced customer-facing, and collaborative teamwork settings, prepared me to effectively understand
-        user needs and translate them into successful technical solutions. From deploying rescue robots to contributing
-        to mental health research, and even developing the Procket, a productivity planning app for procrastinators, my
-        passion for problem-solving and helping others has been the driving force behind my career. Leveraging my skills
-        in the tech world driven by aspiring to blend psychology and tech in meaningful ways. Fueled by my drive for
-        using technology to make a positive impact, I embarked on a self-directed journey into software development.
-        Through a combination of online resources, immersive individual projects, collaborative teamwork projects, and
-        hackathons at a bootcamp, I developed my programming skills and solidified my understanding of core coding
-        practices, design principles, and development processes. This hands-on learning experience equipped me to write
-        clean, efficient code and deliver functional projects within tight deadlines, making me a valuable asset to any
-        software development team.
-      </p>
+      <div className="about__content">
+        <img className="about__slogan" src={baby} alt="baby girl" />
+        <p>
+          I was born with a name meaning "the best" in Persian, yet my path has been one of conquering perfectionism,
+          embracing continuous growth, and cherishing lifelong learning. My career exploration across robotics,
+          psychology, human resources, customer service, and sales has equipped me with a unique blend of analytical
+          thinking, leadership, and interpersonal communication skills. These transferable skillsets, honed through
+          academic, fast-paced customer-facing, and collaborative teamwork settings, prepared me to effectively
+          understand user needs and translate them into successful technical solutions. From deploying rescue robots to
+          contributing to mental health research, and even developing the Procket, a productivity planning app for
+          procrastinators, my passion for problem-solving and helping others has been the driving force behind my
+          career. Leveraging my skills in the tech world driven by aspiring to blend psychology and tech in meaningful
+          ways. Fueled by my drive for using technology to make a positive impact, I embarked on a self-directed journey
+          into software development. Through a combination of online resources, immersive individual projects,
+          collaborative teamwork projects, and hackathons at a bootcamp, I developed my programming skills and
+          solidified my understanding of core coding practices, design principles, and development processes. This
+          hands-on learning experience equipped me to write clean, efficient code and deliver functional projects within
+          tight deadlines, making me a valuable asset to any software development team.
+        </p>
+      </div>
 
       <section className="about__subsection">
         <details>
-          <summary className="about__subsection-title"> Features</summary>
+          <summary className="about__subsection-title">Attributes</summary>
           <ul className="about__subsection-content">
             <li>Quick Learner</li>
             <li>Diligent & Detail-Oriented</li>
